017ce88398d4f0347af77969bb09e33e8e951084
jucobee <jucobee@gmail.com>
2021-11-11 17:04:44 -0800

    added basic grapple mechanic

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -2,10 +2,12 @@ extends KinematicBody2D
 
 export var SPEED = 600.0
 export var ACCELERATION = 20.0
+export var TERMINAL_VELOCITY = 1000.0
 export var AIR_ACCELERATION = 5.0
 export var JUMP_HEIGHT = 200.0
 export var JUMP_PEAK = 0.5 # time to peak of jump
 export var JUMP_DESCENT = 0.4 # time to descend
+export var SHOOT_SPEED = 1000.0
 
 onready var jump_velocity = ((2.0 * JUMP_HEIGHT) / JUMP_PEAK) * -1.0
 onready var jump_gravity = ((-2.0 * JUMP_HEIGHT) / (JUMP_PEAK * JUMP_PEAK)) * -1.0
@@ -13,10 +15,11 @@ onready var fall_gravity = ((-2.0 * JUMP_HEIGHT) / (JUMP_DESCENT * JUMP_DESCENT)
 
 onready var ray_right = $Raycasts/right
 onready var ray_left = $Raycasts/left
+onready var web = $Raycasts/web
 
 enum {
 	MOVE,
-	AIR,
+	GRAPPLE,
 	DASH
 }
 
@@ -24,9 +27,13 @@ var state = MOVE
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
+	web.set_cast_to(get_local_mouse_position())
+	
 	match state:
 		MOVE:
 			move(delta)
+		GRAPPLE:
+			grapple()
 		DASH:
 			pass
 	
@@ -38,7 +45,7 @@ func move(delta):
 	var walled = get_wall()
 	var input = get_input()
 	
-	velocity.y += get_gravity() * delta
+	velocity.y += clamp(get_gravity() * delta, 0, TERMINAL_VELOCITY)
 	
 	if grounded == true:
 		velocity.x = lerp(velocity.x, input * SPEED, ACCELERATION * delta)
@@ -57,6 +64,9 @@ func move(delta):
 	if Input.is_action_just_released("jump") and velocity.y < 0:
 		velocity.y = lerp(velocity.y, 0, ACCELERATION * delta)
 	
+	if Input.is_action_just_pressed("shoot_web"):
+		state = GRAPPLE
+	
 
 func get_input():
 	var horizontal = 0
@@ -68,6 +78,30 @@ func get_input():
 
 func jump():
 	velocity.y = jump_velocity
+
+func grapple():
+	if web.is_colliding():
+		var target = web.get_collider()
+		var shoot_vector = (target.global_position - global_position)
+		velocity = SHOOT_SPEED * shoot_vector.normalized()
+		if shoot_vector.length() < 30:
+			state = MOVE  
+		
+	else:
+		state = MOVE
+
+## WEB SWINGING FUNCTION
+#func web_grapple():
+#	web.set_cast_to(get_local_mouse_position())
+#	if Input.is_action_just_pressed("shoot_web"):
+#		if web.is_colliding():
+#			print("yes")
+#			var target = web.get_collision_point()
+#			var shoot_vector = (target - global_position).normalized()
+#			var distance = global_position.distance_to(target)
+#
+#			velocity = distance * shoot_vector * SHOOT_SPEED
+#
 
 func get_gravity():
 	return jump_gravity if velocity.y < 0.0 else fall_gravity
