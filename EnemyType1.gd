e51faf18a0e1a0b3dbe513bbe302970dae6f9ec2
anthonyk191 <anthony.k.n191@gmail.com>
2021-11-16 20:20:56 -0800

    enemy AI

--- EnemyType1.gd
+++ EnemyType1.gd
@@ -1,7 +1,9 @@
 extends KinematicBody2D
 
-var speed = 20
+var SPEED = 50
 var velocity = Vector2.ZERO
+var FRICTION = 10
+var ACCELERATION = 30
 
 enum{
 	IDLE,
@@ -10,17 +12,27 @@ enum{
 }
 
 #onready var stats = $Stats
+onready var playerDetectionZone = $PlayerDetection
 var state = CHASE
 
 func _physics_process(delta):
 	match state:
 		IDLE:
-			pass
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION *delta)
+			seek_player()
 		WANDER:
 			pass
 		CHASE:
-			pass
+			var player = playerDetectionZone.player
+			if player != null:
+				var direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction * SPEED, ACCELERATION * delta)
+				
+				velocity = move_and_slide(velocity)
 
-func chasePlayer(delta):
-	velocity = 0
+func seek_player():
+	if playerDetectionZone.can_see_player():
+		state = CHASE
+	else:
+		state = WANDER
 	
