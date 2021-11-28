ae7eee09bd2cfe0708624c4df81fdf40653ae054
anthonyk191 <anthony.k.n191@gmail.com>
2021-11-28 13:16:17 -0800

    Wild Enemy

--- EnemyType1.gd
+++ EnemyType1.gd
@@ -1,38 +1,47 @@
 extends KinematicBody2D
 
-var SPEED = 50
+var SPEED = 500
 var velocity = Vector2.ZERO
-var FRICTION = 10
-var ACCELERATION = 30
+var FRICTION = 20
+var ACCELERATION = 250
+var BOUNCE = -20
 
 enum{
 	IDLE,
-	WANDER,
 	CHASE,
 }
 
 #onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetection
-var state = CHASE
+var state = IDLE
 
 func _physics_process(delta):
 	match state:
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION *delta)
+			prevent_collision(delta)
 			seek_player()
-		WANDER:
-			pass
 		CHASE:
+			prevent_collision(delta)
 			var player = playerDetectionZone.player
 			if player != null:
 				var direction = (player.global_position - global_position).normalized()
 				velocity = velocity.move_toward(direction * SPEED, ACCELERATION * delta)
 				
 				velocity = move_and_slide(velocity)
+			seek_player()
 
 func seek_player():
 	if playerDetectionZone.can_see_player():
 		state = CHASE
 	else:
-		state = WANDER
-	
+		state = IDLE
+
+#checking for walls
+func prevent_collision(delta):
+	var collision_close = null
+	$rays.rotation += delta* 11 * PI
+	for ray in $rays.get_children():
+		if ray.is_colliding():
+			var collision_point = ray.get_collision_point() - global_position
+			velocity += collision_point.normalized() * BOUNCE
