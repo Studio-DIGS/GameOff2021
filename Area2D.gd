d48cf888f579fb8e5f7966134aecf06405f7df3e
anthonyk191 <anthony.k.n191@gmail.com>
2021-11-30 17:08:01 -0800

    enemy2 Changes

--- Area2D.gd
+++ Area2D.gd
@@ -3,7 +3,7 @@ extends Area2D
 var player = null
 
 func can_see_player():
-	return player != null
+	return player
 
 func _on_Area2D_area_entered(area):
 	player = area
--- EnemyType2.gd
+++ EnemyType2.gd
@@ -3,17 +3,19 @@ extends StaticBody2D
 onready var find_player = $PlayerDetection
 var bullet = load("res://Bullet.tscn")
 var bullet_Speed = 100
-
+var playerLocation = null
 
 func _physics_process(delta):
-	if find_player.can_see_player() == true:
+	if find_player.can_see_player():
+		playerLocation = find_player.player.global_position
+		attack(playerLocation - global_position)
 		print("true")
-		attack()
-	if find_player.can_see_player() == false:	
+		
+	if find_player.can_see_player() == null:	
 		print("false")
 
-func attack(x):
+func attack(input):
 	var bullet_instanced = bullet.instance()
 	get_parent().add_child(bullet_instanced) # learn this Anthony
 	bullet_instanced.position = global_position
-	bullet_instanced.apply_impulse(Vector2.ZERO, x.normalized * bullet_Speed)
+	bullet_instanced.apply_impulse(Vector2.ZERO, input.normalized() * bullet_Speed)
